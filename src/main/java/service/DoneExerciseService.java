2d5b590cfd56328a306f7f0ec78e25c8c7adbf4f
artur <zieloneimperium230@o2.pl>
2024-02-03 13:08:10 +0100

    commit

--- src/main/java/service/DoneExerciseService.java
+++ src/main/java/service/DoneExerciseService.java
@@ -7,61 +7,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import to.DoneExerciseTo;
 
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class DoneExerciseService {
 
-	private final ModelMapper modelMapper = new ModelMapper();//todo bean
-	private IDoneExerciseDao dao;
+    private final ModelMapper modelMapper = new ModelMapper();//todo bean
+    @PersistenceContext
+    private IDoneExerciseDao dao;
 
-	public DoneExerciseService() {
-	}
+    public DoneExerciseService() {
+    }
 
-	@Autowired
-	public DoneExerciseService(IDoneExerciseDao dao) {
-		this.dao = dao;
-	}
+    @Autowired
+    public DoneExerciseService(IDoneExerciseDao dao) {
+        this.dao = dao;
+    }
 
-	public List<DoneExerciseTo> getDoneExercisesList() {
-		List<DoneExercise> daoList = this.dao.findAll();
-		List<DoneExerciseTo> result = daoList.stream().map(dao -> modelMapper.map(dao, DoneExerciseTo.class)).toList();
-		return result;
-	}
+    public List<DoneExerciseTo> getDoneExercisesList() {
+        List<DoneExercise> daoList = this.dao.findAll();
+        List<DoneExerciseTo> result = daoList.stream().map(dao -> modelMapper.map(dao, DoneExerciseTo.class)).toList();
+        return result;
+    }
 
-	public DoneExerciseTo getDoneExercise(Long doneExerciseId) {
-		Optional<DoneExercise> doneExerciseDao = dao.findById(doneExerciseId);
-		if (doneExerciseDao.isEmpty()) {
-			return null;
-		}
-		DoneExerciseTo doneExerciseTo = modelMapper.map(doneExerciseDao.get(), DoneExerciseTo.class);
-		return doneExerciseTo;
-	}
+    public DoneExerciseTo getDoneExercise(Long doneExerciseId) {
+        Optional<DoneExercise> doneExerciseDao = dao.findById(doneExerciseId);
+        if (doneExerciseDao.isEmpty()) {
+            return null;
+        }
+        DoneExerciseTo doneExerciseTo = modelMapper.map(doneExerciseDao.get(), DoneExerciseTo.class);
+        return doneExerciseTo;
+    }
 
-	public DoneExerciseTo createDoneExercise(DoneExerciseTo newExercise) {
-		DoneExercise doneExerciseDao = modelMapper.map(newExercise, DoneExercise.class);
-		DoneExercise createdDoneExercise = dao.save(doneExerciseDao);
-		return modelMapper.map(createdDoneExercise, DoneExerciseTo.class);
-	}
+    public DoneExerciseTo createDoneExercise(DoneExerciseTo newExercise) {
+        DoneExercise doneExerciseDao = modelMapper.map(newExercise, DoneExercise.class);
+        DoneExercise createdDoneExercise = dao.save(doneExerciseDao);
+        return modelMapper.map(createdDoneExercise, DoneExerciseTo.class);
+    }
 
-	public DoneExerciseTo editDoneExercise(DoneExerciseTo doneExerciseTo) {
-		DoneExercise doneExerciseDao = modelMapper.map(doneExerciseTo, DoneExercise.class);
-		DoneExercise editedDoneExercise = dao.save(doneExerciseDao);
-		return modelMapper.map(editedDoneExercise, DoneExerciseTo.class);
-	}
+    public DoneExerciseTo editDoneExercise(DoneExerciseTo doneExerciseTo) {
+        DoneExercise doneExerciseDao = modelMapper.map(doneExerciseTo, DoneExercise.class);
+        DoneExercise editedDoneExercise = dao.save(doneExerciseDao);
+        return modelMapper.map(editedDoneExercise, DoneExerciseTo.class);
+    }
 
-	public boolean deleteDoneExercise(Long doneExerciseId) {
-		dao.deleteById(doneExerciseId);
-		return !dao.existsById(doneExerciseId);
-	}
+    public boolean deleteDoneExercise(Long doneExerciseId) {
+        dao.deleteById(doneExerciseId);
+        return !dao.existsById(doneExerciseId);
+    }
 
-	public IDoneExerciseDao getDao() {
-		return dao;
-	}
+    public IDoneExerciseDao getDao() {
+        return dao;
+    }
 
-	public void setDao(IDoneExerciseDao dao) {
-		this.dao = dao;
-	}
+    public void setDao(IDoneExerciseDao dao) {
+        this.dao = dao;
+    }
 
 }
